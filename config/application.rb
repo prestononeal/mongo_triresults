cb4a27be5d63d2bc2f6c10b5a322beda6e69f7b4
Preston O'Neal <prestononeal@gmail.com>
2016-06-26 15:33:45 -0700

    Add Mongoid support

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module Triresults
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # bootstraps mongoid within applications
+    Mongoid.load!('./config/mongoid.yml')
   end
 end
