a1c7c7d6da3da0071e6a598caa755c26a4e9f459
Preston O'Neal <prestononeal@gmail.com>
2016-06-26 17:40:59 -0700

    Add 1:1 embedded relationship b/w Racer and RacerInfo

--- app/models/racer_info.rb
+++ app/models/racer_info.rb
@@ -7,4 +7,10 @@ class RacerInfo
   field :g, as: :gender, type: String
   field :yr, as: :birth_year, type: Integer
   field :res, as: :residence, type: Address
+
+  embedded_in :parent, polymorphic: true
+
+  validates_presence_of :first_name, :last_name, :gender, :birth_year
+  validates_inclusion_of :gender, :in => %w( M F )
+  validates_numericality_of :birth_year, :only_integer => true, :less_than => Time.now.year
 end
