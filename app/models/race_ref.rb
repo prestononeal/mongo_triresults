e7537dac7bcb4342d866ba3a14e61cffd0157f91
Preston O'Neal <prestononeal@gmail.com>
2016-06-28 18:31:13 -0700

    Add RaceRef model

---
+++ app/models/race_ref.rb
@@ -0,0 +1,10 @@
+class RaceRef
+  """
+  Represents race-identifying information that is copied into each Entrant.
+  This is built from Entrant attributes.
+  """
+  include Mongoid::Document
+
+  field :n, as: :name, type: String
+  field :date, type: Date
+end
