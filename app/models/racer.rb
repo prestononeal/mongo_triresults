82a398e5d54f4446d4e5ded20513d01a30e5f53b
Preston O'Neal <prestononeal@gmail.com>
2016-06-28 21:38:49 -0700

    Implement 1:M relationship b/w Racer/Entrant

--- app/models/racer.rb
+++ app/models/racer.rb
@@ -7,6 +7,8 @@ class Racer
   store_in collection: "racers"
 
   embeds_one :info, as: :parent, class_name: "RacerInfo", autobuild: true
+  has_many :races, class_name: "Entrant", foreign_key: "racer.racer_id", dependent: :nullify,
+    order: :"race.date".desc
 
   before_create do |racer|
     racer.info.id = racer.id
